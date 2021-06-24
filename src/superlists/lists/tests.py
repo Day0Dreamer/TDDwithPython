d1056f2d6390dba834e37ecea826269b7b423158
DayDreamer <poisonsonic@gmail.com>
2021-06-24 22:46:31 +0200

    3 changed files, plus 2 migrations

--- src/superlists/lists/tests.py
+++ src/superlists/lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.urls import resolve
 from django.db import models
 from django.test import TestCase
@@ -13,15 +13,23 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ItemModelTest(TestCase):
+class ItemAndListModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()  # type: models.Model
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()  # type: models.Model
         second_item.text = "The second list item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -29,7 +37,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "The second list item")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -38,9 +48,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_items(self):
-
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
 
         response = self.client.get("/lists/temporary_unique_url_for_a_list/")
 
@@ -50,15 +60,18 @@ class ListViewTest(TestCase):
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post("/lists/new/", data={"item_text": "A test list item"})
+        list_ = List.objects.create()
+        self.client.post(
+            "/lists/new/", data={"item_text": "A test list item"}, list=list_
+        )
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A test list item")
 
     def test_redirects_after_POST(self):
+        list_ = List.objects.create()
         response = self.client.post(
-            "/lists/new/",
-            data={"item_text": "A test list item"},
+            "/lists/new/", data={"item_text": "A test list item"}, list=list_
         )
         self.assertRedirects(response, "/lists/temporary_unique_url_for_a_list/")
