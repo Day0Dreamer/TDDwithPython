269fa0c313fe7718079da0f0398b12b0aa6a6490
DayDreamer <poisonsonic@gmail.com>
2021-06-24 20:36:37 +0200

    Func tests for multiuser interface

--- src/superlists/superlists/users/tests/functional_tests.py
+++ src/superlists/superlists/users/tests/functional_tests.py
@@ -12,9 +12,12 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self) -> None:
-        chrome_config = Options()
-        chrome_config.headless = True
-        self.browser = webdriver.Chrome(options=chrome_config)
+        self.chrome_config = Options()
+        self.chrome_config.headless = True
+        self.chrome_config.add_experimental_option(
+            "excludeSwitches", ["enable-logging"]
+        )
+        self.browser = webdriver.Chrome(options=self.chrome_config)
 
     def tearDown(self) -> None:
         self.browser.close()
@@ -36,7 +39,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 sleep(0.25)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Go to a web address for the project page
         self.browser.get(self.live_server_url)
 
@@ -56,7 +59,6 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Sell the gold nuggets" as an item in a to-do list table
         input_box.send_keys(Keys.ENTER)
-        sleep(1)
 
         self.wait_for_row_in_list_table("1: Sell the gold nuggets")
 
@@ -64,7 +66,6 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id("id_new_item")
         input_box.send_keys("Wash the golden goose")
         input_box.send_keys(Keys.ENTER)
-        sleep(1)
 
         # Isaac sees two entered items
         self.wait_for_row_in_list_table("1: Sell the gold nuggets")
@@ -77,8 +78,50 @@ class NewVisitorTest(LiveServerTestCase):
         # Isaac visits that URL - her to-do list is still there.
 
         # Satisfied, Isaac goes back to sleep
-        self.fail("Finish the test!")
+        # self.fail("Finish the test!")
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Go to a web address for the project page
+        self.browser.get(self.live_server_url)
 
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
+        input_box = self.browser.find_element_by_id("id_new_item")
+        input_box.send_keys("Sell the gold nuggets")
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Sell the gold nuggets")
+
+        # She enters "Wash the golden goose"
+        input_box = self.browser.find_element_by_id("id_new_item")
+        input_box.send_keys("Wash the golden goose")
+        input_box.send_keys(Keys.ENTER)
+
+        # She notices that her list has a unique URL
+        jane_list_url = self.browser.current_url
+        self.assertRegex(jane_list_url, "/lists/.+")
+
+        # # We use a new browser session to make sure that no information
+        # # of Jane's is coming through from cookies
+        self.browser.quit()
+        self.browser = webdriver.Chrome(options=self.chrome_config)
+
+        # Francis visits the home page. There is no sign of Jane's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Sell the gold nuggets", page_text)
+        self.assertNotIn("golden goose", page_text)
+
+        # Francis starts a new list by entering a new item. He is less interesting than Jane...
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, jane_list_url)
+
+        # Again, there is no trace of Jane's list
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+        # Satisfied, they both go back to sleep
