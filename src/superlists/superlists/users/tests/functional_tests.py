77a9e69a66e3a6ab68f01de9fb35fa3c76437480
DayDreamer <poisonsonic@gmail.com>
2021-06-24 10:06:37 +0200

    Updated the func tests

--- src/superlists/superlists/users/tests/functional_tests.py
+++ src/superlists/superlists/users/tests/functional_tests.py
@@ -1,5 +1,7 @@
+from time import sleep
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,15 +18,30 @@ class NewVisitorTest(unittest.TestCase):
         # Verify that we are on the right page
         # assert 'To-do' in self.browser.title
         self.assertIn('To-do', self.browser.title)
-        # self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
 
         # Isaac sees there is a way to enter a new to do item
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Isaac types 'Sell the gold nuggets' into a line field
+        input_box.send_keys('Sell the gold nuggets')
 
-        # Isaac sees another line to enter another to do item
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Sell the gold nuggets" as an item in a to-do list table
+        input_box.send_keys(Keys.ENTER)
+        sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Sell the gold nuggets' for row in rows))
 
         # Isaac enters "Wash the golden goose"
+        input_box.send_keys('Wash the golden goose')
 
         # Isaac sees two entered items
 
@@ -35,6 +52,7 @@ class NewVisitorTest(unittest.TestCase):
         # Isaac visits that URL - her to-do list is still there.
 
         # Satisfied, Isaac goes back to sleep
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
